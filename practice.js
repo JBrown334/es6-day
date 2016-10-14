c8c019ced08dae4f77f2d1382384f631c3f3f746
Joe Brown <joebrown28@gmail.com>
2016-10-13 21:06:50 -0600

    awesome stuff

--- practice.js
+++ practice.js
@@ -6,20 +6,31 @@
 
 // Rewrite the following line so that inner is scoped to the if statement.
 if ( true ) {
-	var inner = "Inner!";
+	{
+		let inner = "Inner!";
+	}
 }
 
+
+
 // declare a variable named noChange that cannot be re-assigned and give it a value of 100.
+
+const noChange = 100;
 
 
 // declare a new constant named vocab with the value of [ "let", "var", "destructuring", "spread", "rest" ]
 
+const vocab = [ "let", "var", "destructuring", "spread", "rest" ];
+
 
 // change "var" to "const" at index 1 of vocab
+
+vocab[1] = "const";
 
 
 // add "arrow function" to the end of vocab
 
+vocab.push("arrow function");
 
 
 ///////////////////////
@@ -28,11 +39,13 @@ if ( true ) {
 
 // Declare 2 new variables, first and last, and assign them your first and last name.
 
+var first = "Joe";
+var last = "Brown";
 
 
 // Declare the variable fullName and set it equal to your full name using template literals
 
-
+var fullName = "Joe Brown";
 
 //////////////////////
 /* OBJECT LITERALS */
@@ -43,12 +56,26 @@ if ( true ) {
 // and lovesDevMountain should be set to true.
 // learnES6 should be a method created with object method shorthand that returns "This is awesome!"
 
+var me = {
+	first,
+	last,
+	lovesDevMountain: true,
+	learnES6() {
+		return "This is awesome!";
+	}
+};
 
 
 // using computed property names create a new object named flipped where the keys are your first and last name
 // and the values are "first" and "last"
 
+var flipped = {
+	[first]: "first",
+	[last]: "last"
+};
 
+console.log(flipped.Joe);
+console.log(first);
 //////////////////////////////
 /* ASSIGNMENT DESTRUCTURING */
 //////////////////////////////
@@ -63,6 +90,7 @@ const destructuring = {
 // using assignment destructuring create two new variables, objects and arrays,
 // with the values taken from the above object
 
+let { objects, arrays } = destructuring;
 
 
 
@@ -71,6 +99,8 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 
 // using assignment destructuring, create three variables notVar, alsoNotVar, and everythingElse
 // notVar should be "let", alsoNotVar should be "const", and everythingElse should contain the rest of the array.
+
+let [ notVar, alsoNotVar, ...everythingElse ] = vocabAgain
 
 
 
@@ -84,6 +114,8 @@ const lastFive = [ 6, 7, 8, 9, 10 ];
 
 // using the spread operator, create a new array named oneThroughTen.
 // You should use the above arrays (and 2 missing digits of your own)
+
+let oneThroughTen = [...firstThree, 4, 5, ...lastFive];
 
 
 // do not modify
@@ -99,14 +131,14 @@ function whatToUse( past, present, future ) {
 // using the spread operator, pass each element of jsVersions as an individual argument to whatToUse
 // and save the return value in a variable named currentJS.
 
-
+let currentJS = whatToUse(...jsVersions);
 
 ///////////////
 /* FUNCTIONS */
 ///////////////
 
 // modify greeter to greet Anonymous if no name is passed.
-function greeter( name ) {
+function greeter( name = "Anonymous" ) {
 	return `Hi ${ name }!`
 }
 
@@ -114,6 +146,10 @@ function greeter( name ) {
 // write a function named toPower that takes two arguments, a number and an exponent,
 // and returns number to the power of exponent.
 // if no exponent is passed, the exponent should be two
+
+function toPower( num, exp = 2 )  {
+	return Math.pow(num, exp);
+}
 
 
 // using rest parameters, write a function named evenOdd that takes in
